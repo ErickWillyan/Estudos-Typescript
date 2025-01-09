54c5bde766960b1293527f775a03b17a0ccef48f
Erick Willyan <erickwillyan2105@gmail.com>
2025-01-09 13:31:27 -0300

    extends, intersersection and array

---
+++ TypeScript/TypeScriptTools/build/interfaces/array_interface.js
@@ -0,0 +1,8 @@
+"use strict";
+let frontend = {
+    tecnologia: [
+        { id: "12", nome: "ReactJS", descricao: "Biblioteca para criar interfaces" },
+        { id: "43", nome: "VueJs", descricao: "Framework Frontend" }
+    ]
+};
+console.log(frontend.tecnologia);
